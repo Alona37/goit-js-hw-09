1e4ec1dd6e6ad285995d1d5cdbde9c18ed05b91c
Alona37 <113711181+Alona37@users.noreply.github.com>
2023-03-21 14:55:34 +0200

    js-hw-09 finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  bodyColor: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+console.log(refs.bodyColor);
+console.log(refs.btnStart);
+console.log(refs.btnStop);
 
+refs.btnStop.setAttribute('disabled', true);
+
+const INTERVAL_DELAY = 1000;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+refs.btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DELAY);
+
+  refs.btnStart.setAttribute('disabled', true);
+  refs.btnStop.removeAttribute('disabled');
+});
+
+refs.btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.setAttribute('disabled', true);
+});
